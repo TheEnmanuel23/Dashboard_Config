953b5233cdee2d0ad007ad11cfe7aa667e4b469f
Enmanuel Jarquin <enmisac@gmail.com>
2016-06-06 10:23:40 -0600

    modifique los modelos, y los puse conrespecto a los diagramas que hice

--- dashboardConfigApp/models.py
+++ dashboardConfigApp/models.py
@@ -1,35 +1,43 @@
 from django.db import models
 
 # Create your models here.
-class Condicion(models.Model):
-	descripcion = models.CharField(max_length=100)
-	tipo = models.CharField(max_length=5)
 
 class Proyect(models.Model):
-    created = models.DateTimeField(auto_now_add=True, auto_now=False)
     nombre = models.CharField(max_length=100)
+    fechaCreacion = models.DateTimeField(auto_now_add=True, auto_now=False)
 
 class Image(models.Model):
-	proyecto = models.ForeignKey(Proyect)
 	descripcion = models.CharField(max_length=100)
-	image = models.ImageField(upload_to='images/', default='images/none_image.jpg')
+	imagen = models.ImageField(upload_to='images/', default='images/none_image.jpg')
+	proyecto = models.ForeignKey(Proyect)
 
-class DataImage(models.Model):
-	image = models.ForeignKey(Image)
+class Capa(models.Model):
 	descripcion = models.CharField(max_length=140)
-	condicion = models.ForeignKey(Condicion)
-	color = models.CharField(max_length=11)
+	image = models.ForeignKey(Image)
 
-class CustomTypeField(models.Model):
+class TipoCampo(models.Model):
 	descripcion = models.CharField(max_length=10)
 
 class Indicador(models.Model):
 	descripcion = models.CharField(max_length=200)
-	tipo = models.ForeignKey(CustomTypeField)
-	color = models.CharField(max_length=11)
-	compareToValue = models.CharField(max_length=20)
+	tipo = models.ForeignKey(TipoCampo)
+
+class Condicion(models.Model):
+	descripcion = models.CharField(max_length=100)
+	tipo = models.CharField(max_length=5)
 
 class DataImage_Indicador(models.Model):
 	indicador = models.ForeignKey(Indicador)
-	dataImage = models.ForeignKey(DataImage)
+	capa = models.ForeignKey(Capa)
 	valor = models.CharField(max_length=100)
+
+class Condicion_Indicador(models.Model):
+	indicador = models.ForeignKey(Indicador)	
+	codicion = models.ForeignKey(Condicion)
+	valorComparar = models.CharField(max_length=20)
+	color = models.CharField(max_length=11)
+
+class Condicion_Capa(models.Model):
+	capa = models.ForeignKey(Capa)	
+	codicion = models.ForeignKey(Condicion)
+	color = models.CharField(max_length=11)
