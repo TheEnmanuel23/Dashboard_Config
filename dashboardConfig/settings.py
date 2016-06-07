4bcf8097c3845ddd4d8553cf8c4b868f6d8fdfa8
Enmanuel Jarquin <enmisac@gmail.com>
2016-06-07 09:26:45 -0600

    estilice un poco el formulario

--- dashboardConfig/settings.py
+++ dashboardConfig/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboardConfigApp',
     'rest_framework',
-    'material',
-    'material.admin',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE_CLASSES = [
