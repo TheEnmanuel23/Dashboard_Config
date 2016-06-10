1916d37de94486d263679219f9e175d15c4470d9
Enmanuel Jarquin <enmisac@gmail.com>
2016-06-10 12:16:02 -0600

    modifique el render de la lista de projectos

--- dashboardConfigApp/static/js/backbone/views/projectView.js
+++ dashboardConfigApp/static/js/backbone/views/projectView.js
@@ -1,8 +1,16 @@
 ConfigDashboard.Views.ProjectView = Backbone.View.extend({
+	initialize: function(){
+		this.model.on('add', this.render);
+		self = this;
+	},
+	template: _.template($('#allProjectTemplate').html()),
 	render: function(){
-		var data = this.model.toJSON();
-		template = _.template($('#allProjectTemplate').html() )
-		var html = template(data);
-		this.setElement(html);
-	}
+		$('#allProject_table_tbody').empty();
+		var data = self.model.toJSON();
+		data.forEach(function(item){
+			var html = self.template(item);
+			self.setElement(html);
+			self.$el.appendTo('#allProject_table_tbody');
+		})
+	},
 });
--- dashboardConfigApp/static/js/mainAllProject.js
+++ dashboardConfigApp/static/js/mainAllProject.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 	window.collections.allProjectCollection = new ConfigDashboard.Collections.ProjectCollection();
-	window.collections.allProjectCollection.on('add', function(model){
-		var view = new ConfigDashboard.Views.ProjectView({model:model});
-		view.render();
-		view.$el.appendTo('#allProject_table_tbody');
+	window.views.projectViews = new ConfigDashboard.Views.ProjectView({
+		model: window.collections.allProjectCollection
 	});
 	window.collections.allProjectCollection.fetch();
 });
