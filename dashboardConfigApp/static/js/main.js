dc7545f30accdb769799113545bfb7364e810e28
Enmanuel Jarquin <enmisac@gmail.com>
2016-06-23 10:22:19 -0600

    quite codigo en backbone que no estaba ocupando

--- dashboardConfigApp/static/js/main.js
+++ dashboardConfigApp/static/js/main.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
 	window.views.app = new ConfigDashboard.Views.App($('body'));
-    window.collections.layers = new ConfigDashboard.Collections.LayersCollections();
-    window.views.layers = new ConfigDashboard.Views.LayersView({
-        model: window.collections.layers
-    });
 });
